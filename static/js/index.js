fcc3ae786ba7cf5129e9fe4e94aeb9f3e61cf3c9
AirQuick <AirQuick@users.noreply.github.com>
2019-02-11 17:37:56 +0900

    Make user font size setting persist
    https://github.com/JohnMcLear/ep_user_font_size/issues/1#issue-113595655

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 exports.postAceInit = function(name, context){
   var padOuter = $('iframe[name="ace_outer"]').contents();
   var padInner = padOuter.find('iframe[name="ace_inner"]');
+  var padcookie = require('ep_etherpad-lite/static/js/pad_cookie').padcookie;
 
   $('#user-font-size').change(function(){
     var newVal = 20 + parseInt($(this).val()) +"%";
@@ -9,6 +10,14 @@ exports.postAceInit = function(name, context){
     // For now this bodge fix will do
     padInner.contents().find("body").css("line-height", newVal);
     padInner.contents().find("body").css("font-size", newVal);
+
+    padcookie.setPref('userFontSize', $('#user-font-size').val());
   });
+
+  var userFontSizeSetting = padcookie.getPref('userFontSize');
+  if (userFontSizeSetting) {
+    $('#user-font-size').val(userFontSizeSetting);
+    $('#user-font-size').trigger('change');
+  }
 }
 
